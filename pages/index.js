c37eaac86ba17a0e75d2a2cc445b7d76100e3fc1
JacksonJW <jacksonjwatkins@gmail.com>
2021-11-21 00:30:33 -0800

    Fix up center grid and add images

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import rougaroo from '../public/images/rougaroo.png'
+import jdubsrub from '../public/images/jdubsrub.png'
+
 
 export default function Home() {
   return (
@@ -13,43 +16,39 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to J Dub's Rubs!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The best barbecue rubs on earth
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          <li className={styles.card}>
+            <a href="https://nextjs.org/docs">
+              {/* <img src="/images/jdubsrub.png" alt="JDub's rub"></img> */}
+              <Image
+                src={jdubsrub}
+                alt="J Dub's rub"
+                priority
+              />
+              <h2>J Dub's rub</h2>
+              <p>The original cajun flavor</p>
+            </a>
+          </li>
+          <li className={styles.card}>
+            <a href="https://nextjs.org/learn">
+              {/* <img src="/images/rougaroo.png" alt="Rougaroo"></img> */}
+              <Image
+                src={rougaroo}
+                alt="Rougaroo"
+                priority
+              />
+              <h2>Rougaroo</h2>
+              <p>A spicy red pepper taste</p>
+            </a>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -64,6 +63,6 @@ export default function Home() {
           </span>
         </a>
       </footer>
-    </div>
+    </div >
   )
 }
