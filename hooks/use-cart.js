cd97b98ce3cc990518aaa97bfd6c8a656c106a43
JacksonJW <jacksonjwatkins@gmail.com>
2022-04-02 18:18:54 -0700

    remove console.logs in use-cart

--- hooks/use-cart.js
+++ hooks/use-cart.js
@@ -21,14 +21,9 @@ export default function useCart() {
     }
   });
 
-  console.log('cartItems: ');
-  console.log(cartItems);
-
   const subtotal = cartItems.reduce((accumulator, { pricePerUnit, quantity }) => {
     return accumulator + (pricePerUnit * quantity);
   }, 0);
-
-  // console.log('subtotal', subtotal);
 
   const totalItems = cartItems.reduce((accumulator, { quantity }) => {
     return accumulator + quantity;
@@ -39,20 +34,12 @@ export default function useCart() {
       let cart1 = { ...prev };
 
       if (cart1.products[id]) {
-        console.log('cart1 before addtoCart():')
-        console.log(cart1)
         cart1.products[id].quantity = cart1.products[id].quantity + 1;
-        console.log('cart1 after addtoCart():')
-        console.log(cart1)
       } else {
-        console.log('cart1 before addtoCart():')
-        console.log(cart1)
         cart1.products[id] = {
           id,
           quantity: 1
         }
-        console.log('cart1 after addtoCart():')
-        console.log(cart1)
       }
 
       return cart1;
