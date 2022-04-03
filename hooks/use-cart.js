dc715e4f8af89caf7530140ecc555f78b1638f4b
JacksonJW <jacksonjwatkins@gmail.com>
2022-04-02 17:45:18 -0700

    adjust spacing in use-cart hook

--- hooks/use-cart.js
+++ hooks/use-cart.js
@@ -6,67 +6,75 @@ import products from '../products.json'
 
 
 const defaultCart = {
-    products: {}
+  products: {}
 }
 
 export default function useCart() {
 
-    const [cart, updateCart] = useState(defaultCart);
+  const [cart, updateCart] = useState(defaultCart);
 
-    const cartItems = Object.keys(cart.products).map(key => {
-        const product = products.find(({ id }) => `${id}` === `${key}`);
-        return {
-            ...cart.products[key],
-            pricePerUnit: product.price
-        }
-    });
-
-    console.log(cartItems);
-
-    const subtotal = cartItems.reduce((accumulator, { pricePerUnit, quantity }) => {
-        return accumulator + (pricePerUnit * quantity);
-    }, 0);
-
-    console.log('subtotal', subtotal);
-
-    const totalItems = cartItems.reduce((accumulator, { quantity }) => {
-        return accumulator + quantity;
-    }, 0);
-
-    function addToCart({ id }) {
-        updateCart((prev) => {
-            let cart = { ...prev };
-
-            if (cart.products[id]) {
-                cart.products[id].quantity = cart.products[id].quantity + 1;
-            } else {
-                cart.products[id] = {
-                    id,
-                    quantity: 1
-                }
-            }
-
-            return cart;
-        })
-    }
-
-    function checkout() {
-        initiateCheckout({
-            lineItems: cartItems.map(({ id, quantity }) => {
-                return {
-                    price: id,
-                    quantity
-                }
-            })
-        })
-    }
-
+  const cartItems = Object.keys(cart.products).map(key => {
+    const product = products.find(({ id }) => `${id}` === `${key}`);
     return {
-        cart,
-        updateCart,
-        subtotal,
-        totalItems,
-        addToCart,
-        checkout
+      ...cart.products[key],
+      pricePerUnit: product.price
     }
+  });
+
+  console.log('cartItems: ');
+  console.log(cartItems);
+
+  const subtotal = cartItems.reduce((accumulator, { pricePerUnit, quantity }) => {
+    return accumulator + (pricePerUnit * quantity);
+  }, 0);
+
+  // console.log('subtotal', subtotal);
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity;
+  }, 0);
+
+  function addToCart({ id }) {
+    updateCart((prev) => {
+      let cart1 = { ...prev };
+
+      if (cart1.products[id]) {
+        console.log('cart1 before addtoCart():')
+        console.log(cart1)
+        cart1.products[id].quantity = cart1.products[id].quantity + 1;
+        console.log('cart1 after addtoCart():')
+        console.log(cart1)
+      } else {
+        console.log('cart1 before addtoCart():')
+        console.log(cart1)
+        cart1.products[id] = {
+          id,
+          quantity: 1
+        }
+        console.log('cart1 after addtoCart():')
+        console.log(cart1)
+      }
+
+      return cart1;
+    })
+  }
+
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map(({ id, quantity }) => {
+        return {
+          price: id,
+          quantity
+        }
+      })
+    })
+  }
+
+  return {
+    cart,
+    subtotal,
+    totalItems,
+    addToCart,
+    checkout
+  }
 }
