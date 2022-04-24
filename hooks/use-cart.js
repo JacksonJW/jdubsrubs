68baa8858c3f21ab5238fbcc3122f9d101a50400
JacksonJW <jacksonjwatkins@gmail.com>
2022-04-23 20:36:43 -0700

    integrate useEffect hook with local storage

--- hooks/use-cart.js
+++ hooks/use-cart.js
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from 'react';
+import { useState, createContext, useContext, useEffect } from 'react';
 
 import { initiateCheckout } from '../lib/payments.js'
 
@@ -14,6 +14,19 @@ export const CartContext = createContext();
 export function useCartState() {
 
   const [cart, updateCart] = useState(defaultCart);
+
+  useEffect(() => {
+    const stateFromStorage = window.localStorage.getItem('jdubsrubs_cart');
+    const data = stateFromStorage && JSON.parse(stateFromStorage);
+    if (data) {
+      updateCart(data);
+    }
+  }, [])
+
+  useEffect(() => {
+    const data = JSON.stringify(cart);
+    window.localStorage.setItem('jdubsrubs_cart', data);
+  }, [cart])
 
   const cartItems = Object.keys(cart.products).map(key => {
     const product = products.find(({ id }) => `${id}` === `${key}`);
